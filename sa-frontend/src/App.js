1e7526ec5910de229095a295f6ce66893129de74
Xiaobai Jiang <xiaobaij@andrew.cmu.edu>
2025-02-05 18:03:08 -0500

    deploy on k8s

--- sa-frontend/src/App.js
+++ sa-frontend/src/App.js
@@ -20,7 +20,7 @@ class App extends Component {
     };
 
     analyzeSentence() {
-        fetch('http://localhost:8080/sentiment', {
+        fetch('http://34.123.248.169:80/sentiment', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
